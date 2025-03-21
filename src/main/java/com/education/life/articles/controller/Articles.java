4c616bcba1875f5e125097aac45da5203c28dad6
Mike Kluev <vicofficialvic@gmail.com>
2025-03-21 07:37:41 +0300

    feat(profile, articles) LIFE-246 Создать переход на страницы Люди, Статьи

---
+++ src/main/java/com/education/life/articles/controller/Articles.java
@@ -0,0 +1,16 @@
+package com.education.life.articles.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class Articles {
+
+    @GetMapping("profile/articles")
+    public String showArticlesForm(HttpSession session, Model model) {
+	return "articlesMain";
+    }
+}
---
+++ src/main/java/com/education/life/subscriptonPeople/controller/People.java
@@ -0,0 +1,15 @@
+package com.education.life.subscriptonPeople.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class People {
+    @GetMapping("profile/subscription/users")
+    public String showPeopleForm(HttpSession httpSession, Model model) {
+	return "people";
+    }
+}
